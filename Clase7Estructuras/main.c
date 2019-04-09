fb989e015fdf537499cfe9ca626f89b1d5a34c09
alumno <alumno@utnfra.com.ar>
2019-04-09 12:21:55 -0300

    Ordenar el desastre

--- Clase7Estructuras/main.c
+++ Clase7Estructuras/main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct eEmpleado /*typedef struct*/
+{
+    int legajo;
+    char nombre[20];
+    char sexo;
+    float sueldoBruto;
+    float sueldoNeto;
+} /*eEmpleado*/;
+
 int main()
 {
+    struct eEmpleado unEmpleado; //no pondria struct de la forma del comentario;
+    struct eEmpleado otroEmpleado;
+    printf("Ingrese legajo");
+    scanf("%d" , &unEmpleado.legajo);
+    printf("Ingrese nombre");
+    fflush(stdin);
+    gets(unEmpleado.nombre);
+    printf("Ingrese sexo");
+    fflush(stdin);
+    scanf("%c" , &unEmpleado.sexo);
+    printf("Ingrese sueldo bruto");
+    scanf("%f" , &unEmpleado.sueldoBruto);
+    unEmpleado.sueldoBruto = unEmpleado.sueldoBruto*0.85;
+
+    otroEmpleado = unEmpleado; //se puede hacer sin importar que este algo adentro
+    unEmpleado.legajo = 7000;//mirar esto y sus efectos al ejecutar
+
+    printf("%d--%s--%c--%f--%f--\n" , unEmpleado.legajo , unEmpleado.nombre , unEmpleado.sexo , unEmpleado.sueldoBruto , unEmpleado.sueldoNeto);
+    printf("%d--%s--%c--%f--%f--\n" , otroEmpleado.legajo , otroEmpleado.nombre , otroEmpleado.sexo , otroEmpleado.sueldoBruto , otroEmpleado.sueldoNeto);
+
+    //puts(unEmpleado.nombre);
+
 
     return 0;
 }
