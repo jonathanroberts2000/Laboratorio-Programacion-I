931d2fd86ccf7ed69dae3132e82261cf03cd75e8
jonathanroberts2000 <jonathanroberts992@gmail.com>
2019-04-22 16:08:18 -0300

    Proyecto c/funciones genericas p/parcial

---
+++ BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int pedirEntero(char mensaje[])
+{
+    int numeroEntero;
+    printf("%s" , mensaje);
+    scanf("%d" , &numeroEntero);
+    return numeroEntero;
+}
+
+float pedirFlotante(char mensaje[])
+{
+    float numeroFlotante;
+    printf("%s" , mensaje);
+    scanf("%f" , &numeroFlotante);
+    return numeroFlotante;
+}
+
+char pedirCaracter(char mensaje[])
+{
+    char caracter;
+    printf("%s" , mensaje);
+    scanf("%c" , &caracter);
+    return caracter;
+}
+
+void recorrerVector(int vector_1[], int tam)
+{
+    int i;
+    for(i=0;i<tam;i++)
+    {
+        printf("%d" , vector_1[i]);
+    }
+}
+
+int generarNumeroRandom()
+{
+    int numero;
+    srand(time(NULL));
+    numero = rand();
+    return numero;
+}
---
+++ BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int pedirEntero(char mensaje[]);
+float pedirFlotante(char mensaje[]);
+char pedirCaracter(char mensaje[]);
+void recorrerVector(int vector_1[], int tam);
+int generarNumeroRandom(void);
