238d25396a59dbdb3755fc8a463e748c5cf2ac35
Jonathan Esteban Roberts <jonathanroberts992@gmail.com>
2019-04-29 23:33:22 -0300

    Proyecto c/funciones genericas p/parcial

--- BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas.c
+++ BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas.c
@@ -42,3 +42,61 @@ int generarNumeroRandom()
     numero = rand();
     return numero;
 }
+
+void buscarMaximo(int vector_1[], int tam)
+{
+    int i;
+    int maximo;
+    int flag = 0;
+    for(i=0;i<tam;i++)
+    {
+        if(vector_1[i]>maximo || flag == 0)
+        {
+            maximo = vector_1[i];
+            flag = 1;
+        }
+    }
+    printf("%d" , maximo);
+}
+
+float sumaNumeros(float num1, float num2)
+{
+    float resultado;
+    resultado = num1 + num2;
+    return resultado;
+}
+
+float restaNumeros(float num1, float num2)
+{
+    float resultado;
+    resultado = num1 - num2;
+    return resultado;
+}
+
+float divisionNumeros(float num1, float num2)
+{
+    float resultado;
+    resultado = num1 / num2;
+    return resultado;
+}
+
+float multiplicacionNumeros(float num1, float num2)
+{
+    float resultado;
+    resultado = num1 * num2;
+    return resultado;
+}
+
+int factorialNumero(int num1)
+{
+    int resultado;
+    if(num1 == 0 || num1 == 1)
+    {
+        resultado = 1;
+    }else
+    {
+        resultado = num1 * factorialNumero(num1 -1);
+    }
+    return resultado;
+}
+
--- BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas.h
+++ BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas/BibliotecaFuncionesGenericas.h
@@ -2,8 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef struct
+{
+    char nombre[50];
+    char apellido[50];
+
+}eEmpleados;
+
 int pedirEntero(char mensaje[]);
 float pedirFlotante(char mensaje[]);
 char pedirCaracter(char mensaje[]);
 void recorrerVector(int vector_1[], int tam);
 int generarNumeroRandom(void);
+void buscarMaximo(int vector_1[], int tam);
+float sumaNumeros(float num1, float num2);
+float restaNumeros(float num1, float num2);
+float divisionNumeros(float num1, float num2);
+float multiplicacionNumeros(float num1, float num2);
