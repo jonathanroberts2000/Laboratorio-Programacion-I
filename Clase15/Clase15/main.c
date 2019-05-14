588a11d47090a88f98c2b0e327125b17343b3906
Jonathan Roberts <jonathanroberts992@gmail.com>
2019-05-14 10:42:35 -0300

    Clase 15 #1 Punteros

--- Clase15/Clase15/main.c
+++ Clase15/Clase15/main.c
@@ -3,13 +3,22 @@
 
 int main()
 {
-    int *x = NULL;
+    /*int *x = NULL;
     int v = 10;
     x = &v;
     printf("%d \n", v);
     printf("%p \n", &v);//direccion de memoria de v
     printf("%p \n", &x);//direccion de memoria de x
     printf("%p \n", x);//el valor de x
-    printf("%d \n", *x);//el valor al que apunta
+    printf("%d \n", *x);//el valor al que apunta*/
+    //pedir un char y cargarlo por puntero
+    char letra;
+    char* pLetra = NULL;
+    pLetra = &letra;
+    printf("Ingrese caracter: ");
+    fflush(stdin);
+    scanf("%c", pLetra);
+    //printf("%c",*pLetra);
+    printf("%c \n", letra);
     return 0;
 }
