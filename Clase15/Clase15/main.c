f34d3c5f74a42bf7e256b5c12c18aa9f091b59e2
Jonathan Roberts <jonathanroberts992@gmail.com>
2019-05-14 12:28:43 -0300

    Clase15

--- Clase15/Clase15/main.c
+++ Clase15/Clase15/main.c
@@ -11,14 +11,30 @@ int main()
     printf("%p \n", &x);//direccion de memoria de x
     printf("%p \n", x);//el valor de x
     printf("%d \n", *x);//el valor al que apunta*/
+
     //pedir un char y cargarlo por puntero
-    char letra;
+
+    /*char letra;
     char* pLetra = NULL;
     pLetra = &letra;
     printf("Ingrese caracter: ");
     fflush(stdin);
     scanf("%c", pLetra);
     //printf("%c",*pLetra);
-    printf("%c \n", letra);
+    printf("%c \n", letra);*/
+
+    int p;
+    int* q;
+    int* r;
+    p = 10;
+    q = &p;
+    r = q;
+    //los dos punteros apuntan al mismo lugar
+    //cuando muestro su contenido apuntan a la misma direccion de memoria
+    printf("%p \n", q);
+    printf("%p \n", r);
+    printf("%d \n", *q);
+    printf("%d \n", *r);
+    printf("%p", &p);
     return 0;
 }
