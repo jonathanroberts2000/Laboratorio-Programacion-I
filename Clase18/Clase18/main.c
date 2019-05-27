2a484a3ae320f64fe294b17092bced2c375cb23c
Jonathan Esteban Roberts <47385932+jonathanroberts2000@users.noreply.github.com>
2019-05-27 11:03:59 -0300

    Add files via upload

---
+++ Clase18/Clase18/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    /*FILE* miArchivo;
+    char nombre[20] = "Jonathan";
+    miArchivo = fopen("D:\\misdatos.txt", "w");
+    if(miArchivo != NULL)
+    {
+        fprintf(miArchivo,"Su nombre es: \n%s" ,nombre);//funciona igual q un printf()
+    }else
+    {
+        printf("No existe el archivo!\n");
+    }
+    fclose(miArchivo);*/
+    FILE* miArchivo;
+    char nombre[20];
+    miArchivo = fopen("D:\\misdatos.txt", "r");
+    fgets(nombre, 30, miArchivo);
+    fgets(nombre, 30, miArchivo);
+    fclose(miArchivo);
+    printf("%s", nombre);
+    //fgets lee la primer linea
+    return 0;
+}
